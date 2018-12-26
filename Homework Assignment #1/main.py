d4c9c3da70e7f251d4205e9582d68c9eaf6f103c
littlebuddha16 <45532044+littlebuddha16@users.noreply.github.com>
2018-12-26 23:04:29 +0530

    Create main.py

---
+++ Homework Assignment #1/main.py
@@ -0,0 +1,46 @@
+"""
+	Homework Assignment #1
+	Variables
+	Favourite Song
+							"""
+
+song = 'Nagumomu Ganaleni' 
+# The Song's about Lord Rama
+
+genre = 'Carnatic' 
+"""
+An ancient South Indian classical music.
+Watch https://www.youtube.com/watch?v=ZnObqYfb-0M&t=1s to understand the music form. 
+
+"""
+
+language = 'Telugu' 
+# One of the major Dravidian languages
+
+composedBy = 'Tyagaraja' 
+# A renowned composer of Carnatic music
+
+raga = 'Abheri' 
+# Rage quite literally translates to colouring, a melodic framework akin to melodic mode; It gives emotion.
+
+artist = 'Tyagaraja himself and several other artists after him' 
+# The song is at least 200 years old and it's sung by several artists over time
+
+lengthOfTheSong = 6 
+# Actually there's no limit on the length of the song; I have seen artists who sung in three minutes and also for an hour and it won't be a loop, that's the beauty of Carnatic music
+
+releaseDate = '1767 - 1847'
+ # The exact date is unknown, it's pretty old song
+
+bestVersion = 'Nagumomu Ganaleni by Kartik' 
+# It's short and it's a fusion of Carnatic and Western music
+
+print('Favourite Song: ' + song)
+print('Genre: ' + genre)
+print('Language: ' + language)
+print('Composed By: ' + composedBy)
+print('Raga: ' + raga)
+print('Artist: ' + artist)
+print('Length: ' + str(lengthOfTheSong) + ' minutes on an average')
+print('Release: ' + releaseDate)
+print('Best Version: ' + bestVersion)
